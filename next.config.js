9fb382c05d4311b658f20c6484c6e4f49b5ce5a1
Your Name <you@example.com>
2025-10-20 00:48:11 -0500

    Redirect /manifest.json to /manifest.webmanifest for PWA cache compatibility

--- next.config.js
+++ next.config.js
@@ -5,4 +5,14 @@ const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
 });
 
-module.exports = withPWA({});
+module.exports = withPWA({
+  async redirects() {
+    return [
+      {
+        source: "/manifest.json",
+        destination: "/manifest.webmanifest",
+        permanent: true,
+      },
+    ];
+  },
+});
